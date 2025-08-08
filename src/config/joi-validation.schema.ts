dafa5f6ee5b4627207c2f78d52c5e3e17ee4b92f
priveemaison07-lgtm <priveemaison07@gmail.com>
2025-08-08 14:23:00 +0100

    Update joi-validation.schema.ts

--- src/config/joi-validation.schema.ts
+++ src/config/joi-validation.schema.ts
@@ -13,6 +13,7 @@ export const JoiValidationSchema = Joi.object({
   DB_PASSWORD: Joi.string().required(),
   DB_NAME: Joi.string().required(),
 
+  
   // JWT
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRATION: Joi.string().default('7d'),
